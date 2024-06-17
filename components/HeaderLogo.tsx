54f0efacf4e0987e8440debaafe03da7ac232ca8
Skillz <drzishan12@gmail.com>
2024-06-17 13:47:24 -0500

    fixup header & tabs

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const HeaderLogo = () => {
   return (
     <div className="relative" style={{ top: "10px" }}>
-      <Image src="/images/Logo.png" alt="Logo" width={50} height={25} />
+      <Image src="/images/Logo.jpg" alt="Logo" width={50} height={25} />
     </div>
   );
 };
